b312753a96e4080f04e728daaaa003d906eb20da
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-06-19 13:52:56 +0530

    Trying to add eventlistners

---
+++ index.js
@@ -0,0 +1,13 @@
+
+for (var i=0;i<querySelectorAll(".drum").length;i++){
+    
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+function handleClick(){
+    alert("i got clicked");
+
+}
+    
+}
+
