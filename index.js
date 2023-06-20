52444d618c6f915fb6c9daf586cefaee5a009c9b
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-06-20 09:14:06 +0530

    updated for loop with buttonAnimation

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i=0;i<querySelectorAll(".drum").length;i++){
-    
-    
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-function handleClick(){
-    alert("i got clicked");
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
 
 }
-    
-}
-
